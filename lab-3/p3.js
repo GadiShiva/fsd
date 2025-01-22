c99624b0c8b70cb29ea2b98097aac4d7abc9c975
GadiShiva <gadishiva52@gmail.com>
2025-01-22 12:04:52 +0530

    code saved sucessfully

---
+++ lab-3/p3.js
@@ -0,0 +1,12 @@
+
+function delayedMessage(msg,delay,callback){
+    setTimeout(()=>{
+        console.log(msg)
+        callback(delay);
+    },delay)
+}
+function Message(delay){
+    console.log(`Message is printed after delay time:${delay}`)
+}
+delayedMessage("Hello CVR!",1000,Message)
+
